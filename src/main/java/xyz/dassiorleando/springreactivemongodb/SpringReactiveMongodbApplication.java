7325916ebc39b8b5db4d45697c3496a212591d08
Dassi Orleando <me@dassiorleando.xyz>
2018-03-28 11:49:53 +0100

    Initial files

---
+++ src/main/java/xyz/dassiorleando/springreactivemongodb/SpringReactiveMongodbApplication.java
@@ -0,0 +1,12 @@
+package xyz.dassiorleando.springreactivemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveMongodbApplication.class, args);
+	}
+}
